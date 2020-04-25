07f544ede8d5c444f435eee993ea649acc350d2d
NITSAN Technologies <sanjay@nitsan.in>
2020-04-25 17:04:30 +0530

    Released 2.0.0
    [Update] Made it compatible with TYPO3 10
    [Feature] Made compatible with ZOHO API V2
    [Improved] Code clean-up and regular maintenance

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Zoho CRM Plugin',
-    'description' => 'Easily install and configure your powermail form with zoho CRM, read more at documentation. Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-zoho-crm-free/',
+    'description' => 'Easily install and configure your powermail form with zoho CRM, read more at documentation. Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/zoho-crm-typo3-extension-free',
     'category' => 'plugin',
-    'author' => 'T3: Keval Pandya, QA: Siddharth Sheth',
+    'author' => 'T3: Milan Rathod, QA: Siddharth Sheth',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.3.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '6.2.0-10.9.99',
